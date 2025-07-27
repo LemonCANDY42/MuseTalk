bd33b27dbe10cde3566541eb0daf1a784a85d454
LianfeiYU <17861904503@163.com>
2025-07-27 20:09:22 +0800

    更新train.sh脚本以支持内存分析选项，修改stage1.yaml中的模型名称和学习率预热步骤，调整检查点保存间隔以优化训练过程。

--- train.sh
+++ train.sh
@@ -2,18 +2,19 @@
 
 # MuseTalk Training Script
 # This script combines both training stages for the MuseTalk model
-# Usage: sh train.sh [stage1|stage2]
-# Example: sh train.sh stage1  # To run stage 1 training
-# Example: sh train.sh stage2  # To run stage 2 training
+# Usage: sh train.sh [stage1|stage2] [true|false]
+# Example: sh train.sh stage1 true  # To run stage 1 training with memory profiling
+# Example: sh train.sh stage2 false  # To run stage 2 training without memory profiling
 
 # Check if stage argument is provided
 if [ $# -ne 1 ]; then
-    echo "Error: Please specify the training stage"
-    echo "Usage: ./train.sh [stage1|stage2]"
+    echo "Error: Please specify the training stage and memory profiling option"
+    echo "Usage: ./train.sh [stage1|stage2] [true|false]"
     exit 1
 fi
 
 STAGE=$1
+USE_SCALENE=$2
 
 # Validate stage argument
 if [ "$STAGE" != "stage1" ] && [ "$STAGE" != "stage2" ]; then
@@ -27,8 +28,17 @@ fi
 # train.py: Training script
 # --config: Path to the training configuration file
 echo "Starting $STAGE training..."
-accelerate launch --config_file ./configs/training/gpu.yaml \
-                  --main_process_port 29502 \
-                  train.py --config ./configs/training/$STAGE.yaml
+
+if [ "$USE_SCALENE" = "true" ]; then
+    echo "Using Scalene for memory profiling"
+    accelerate launch --config_file ./configs/training/gpu.yaml \
+            --main_process_port 29502  \
+            --no_python scalene --gpu --memory  --profile-exclude lib/python3 --html --outfile train_profile.html --- train.py --config ./configs/training/$STAGE.yaml
+else
+    echo "Using Accelerate for training"
+    accelerate launch --config_file ./configs/training/gpu.yaml \
+                      --main_process_port 29502 \
+                      train.py --config ./configs/training/$STAGE.yaml
+fi
 
 echo "Training completed for $STAGE" 
