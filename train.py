f5a0f9bc00c247c95c00198ac0bc456a6d798d00
LianfeiYU <17861904503@163.com>
2025-08-29 00:08:03 +0800

    fix

--- train.py
+++ train.py
@@ -456,7 +456,7 @@ def main(cfg):
                         print("sync_loss is nan, set it to 0")
                         sync_loss = last_sync_loss.clone().detach().requires_grad_(True)
                     else:
-                        last_sync_loss = sync_loss.detech().clone()
+                        last_sync_loss = sync_loss.detach().clone()
                     sync_loss_accum += sync_loss.item()
                     
                     loss += sync_loss * cfg.loss_params.sync_loss * adapted_weight
